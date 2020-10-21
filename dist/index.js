639c8e643f196f4cb1d8c3ed0038066350ee1105
getlarge <ed@getlarge.eu>
2020-10-21 17:45:09 +0200

    remove build files

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var generate_interfaces_1 = require("./generate-interfaces");
-Object.defineProperty(exports, "generate", { enumerable: true, get: function () { return generate_interfaces_1.generate; } });
-var validate_schemas_1 = require("./validate-schemas");
-Object.defineProperty(exports, "validate", { enumerable: true, get: function () { return validate_schemas_1.validate; } });
-var utils_1 = require("./utils");
-Object.defineProperty(exports, "loadExternalSchema", { enumerable: true, get: function () { return utils_1.loadExternalSchema; } });
