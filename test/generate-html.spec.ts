f1978893c1246fb3f4763e92313cbef7d386322a
getlarge <ed@getlarge.eu>
2020-10-28 14:33:56 +0100

    add test

--- test/generate-html.spec.ts
+++ test/generate-html.spec.ts
@@ -2,17 +2,25 @@ import { expect } from 'chai';
 import { readFileSync } from 'fs';
 import { generateHtml } from '../src/generate-html';
 
-const templatePath = `https://raw.githubusercontent.com/s1seven/schemas/main/EN10168.hbs`;
 const schemaPath = `${__dirname}/../fixtures/EN10168/valid_en10168_test.json`;
 
-describe.skip('GenerateHTML', () => {
-  const expectedHtml = readFileSync(
-    `${__dirname}/../fixtures/EN10168/index.html`,
+describe('GenerateHTML', () => {
+  const expectedHtmlFromHbs = readFileSync(
+    `${__dirname}/../fixtures/EN10168/template_hbs.html`,
+    'utf-8'
+  );
+  const expectedHtmlFromMjml = readFileSync(
+    `${__dirname}/../fixtures/EN10168/template_mjml.html`,
     'utf-8'
   );
 
-  it('should generate HTML certificate using external template and JSON paths', async () => {
-    const html = await generateHtml(templatePath, schemaPath);
-    expect(html).to.be.equal(expectedHtml);
+  it('should render HTML certificate using certificate local path and HBS template', async () => {
+    const html = await generateHtml(schemaPath);
+    expect(html).to.be.equal(expectedHtmlFromHbs);
+  });
+
+  it.skip('should render HTML certificate using certificate local path and MJML template', async () => {
+    const html = await generateHtml(schemaPath, { templateType: 'mjml' });
+    expect(html).to.be.equal(expectedHtmlFromMjml);
   });
 });
