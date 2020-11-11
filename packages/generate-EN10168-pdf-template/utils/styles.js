9ca3e8366b5ee8b84a6dc84ec8432cec4bf2e730
getlarge <ed@getlarge.eu>
2020-11-11 15:44:21 +0100

    fix: update example as JS files set

---
+++ packages/generate-EN10168-pdf-template/utils/styles.js
@@ -0,0 +1,50 @@
+module.exports = {
+  table: {
+    font: 'Lato',
+    fontSize: 8,
+    margin: [0, 4, 0, 4],
+    light: true,
+  },
+  footer: {
+    font: 'Lato',
+    fontSize: 8,
+  },
+  h1: {
+    font: 'Lato',
+    bold: true,
+    fontSize: 20,
+    margin: [0, 16, 0, 8],
+    decoration: 'underline',
+    alignment: 'center',
+  },
+  h2: {
+    font: 'Lato',
+    bold: true,
+    decoration: 'underline',
+    fontSize: 16,
+    margin: [0, 8, 0, 4],
+  },
+  p: {
+    fontSize: 10,
+    margin: [0, 4, 0, 4],
+  },
+  caption: {
+    font: 'Lato',
+    fontSize: 8,
+    margin: [0, 4, 0, 4],
+    light: true,
+  },
+  tableHeader: {
+    font: 'Lato',
+    fontSize: 10,
+    italics: true,
+    margin: [0, 4, 0, 4],
+  },
+  margin52: {
+    margin: [5, 2, 5, 2],
+  },
+  CEstyle: {
+    alignment: 'center',
+    margin: [0, 50, 0, 0],
+  },
+};
