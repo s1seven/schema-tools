18fa326c2b0fb30661a0fb2e0d9c756c82c06b93
Michał Ziętalak <michal.z@ethworks.io>
2020-11-12 16:33:46 +0100

    Add tests for measurement

--- packages/generate-EN10168-pdf-template/test/measurement.spec.ts
+++ packages/generate-EN10168-pdf-template/test/measurement.spec.ts
@@ -6,10 +6,35 @@ import certificate from '../../../fixtures/EN10168/v0.0.2/valid_cert.json';
 const defaultSchemaUrl = certificate.RefSchemaUrl || 'https://schemas.en10204.io/en10168-schemas/v0.0.2/schema.json';
 
 describe('Rendering measurement', () => {
-  it('', async () => {
+  it('works for example certificate', async () => {
     const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
     const i18n = new Translate(translations);
-    const measruements = measurement(certificate.Certificate.ProductDescription.B10, 'B10', i18n);
-    console.log(measruements);
+    const measurements = measurement(certificate.Certificate.ProductDescription.B10, 'B10', i18n);
+    const expected = [
+      { text: 'B10 Length / Länge ', style: 'tableHeader' },
+      {},
+      {},
+      { aling: 'justify', columns: [{ text: '1200 mm' }, { text: '' }, { text: '' }] },
+    ];
+    expect(measurements[0]).toEqual(expected);
+  });
+  it('renders correctly with Maximun, Minimum and Property', async () => {
+    const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
+    const i18n = new Translate(translations);
+    const input = {
+      Value: 200,
+      Minimum: 100,
+      Maximum: 350,
+      Unit: 'mm',
+      Property: 'LengthProperty',
+    };
+    const measurements = measurement(input, 'B10', i18n);
+    const expected = [
+      { text: 'B10 Length / Länge LengthProperty', style: 'tableHeader' },
+      {},
+      {},
+      { aling: 'justify', columns: [{ text: '200 mm' }, { text: 'min 100 mm' }, { text: 'max 350 mm' }] },
+    ];
+    expect(measurements[0]).toEqual(expected);
   });
 });
