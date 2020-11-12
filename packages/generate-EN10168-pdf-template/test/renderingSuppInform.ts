0045357daa766ac74524af76eb5967116f7b44d9
getlarge <ed@getlarge.eu>
2020-11-12 07:48:29 +0100

    fix: update tests

--- packages/generate-EN10168-pdf-template/test/renderingSuppInform.ts
+++ packages/generate-EN10168-pdf-template/test/renderingSuppInform.ts
@@ -1,80 +1,104 @@
 import { expect } from 'chai';
-
-import { Translate } from '../utils/translate';
+import { getTranslations } from '../src/lib/helpers';
+import { Translate } from '../src/lib/translate';
 import { supplementaryInformation } from '../src/lib/supplementaryInformation';
 
+const defaultSchemaUrl = 'https://schemas.en10204.io/en10168-schemas/v0.0.2/schema.json';
+
 describe('Rendering supplementary information', () => {
-  it('does not render header when no supplementary inforamtion is given', () => {
-    const i18n = new Translate(['EN', 'DE']);
-    expect(supplementaryInformation({}, i18n)).to.be.eql([])
+  it('does not render header when no supplementary inforamtion is given', async () => {
+    const translations = await getTranslations(['EN'], defaultSchemaUrl);
+    const i18n = new Translate(translations);
+    expect(supplementaryInformation({}, i18n)).to.be.eql([]);
   });
-  it('correctly renders header in one language', () => {
+
+  it('correctly renders header in one language', async () => {
     const suppInfo = {
-      "A11": {
-        "Key": "First Supplementary Information Commercial Transaction",
-        "Value": "1.0",
-        "Unit": "Apples"
+      A11: {
+        Key: 'First Supplementary Information Commercial Transaction',
+        Value: '1.0',
+        Unit: 'Apples',
       },
-    }
-    const i18n = new Translate(['EN']);
+    };
+    const translations = await getTranslations(['EN'], defaultSchemaUrl);
+    const i18n = new Translate(translations);
     expect(supplementaryInformation(suppInfo, i18n)[0]).to.be.eql([
-      { text: 'Supplementary information', style: 'h2', colSpan: 3 }, {}, {},
-    ])
+      { text: 'Supplementary information', style: 'h2', colSpan: 3 },
+      {},
+      {},
+    ]);
   });
-  it('correctly renders header in two languages', () => {
+
+  it('correctly renders header in two languages', async () => {
     const suppInfo = {
-      "A11": {
-        "Key": "First Supplementary Information Commercial Transaction",
-        "Value": "1.0",
-        "Unit": "Apples"
+      A11: {
+        Key: 'First Supplementary Information Commercial Transaction',
+        Value: '1.0',
+        Unit: 'Apples',
       },
-    }
-    const i18n = new Translate(['EN', 'DE']);
+    };
+    const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
+    const i18n = new Translate(translations);
     expect(supplementaryInformation(suppInfo, i18n)[0]).to.be.eql([
-      { text: 'Supplementary information / Ergänzende Angaben', style: 'h2', colSpan: 3 }, {}, {},
-    ])
+      { text: 'Supplementary information / Ergänzende Angaben', style: 'h2', colSpan: 3 },
+      {},
+      {},
+    ]);
   });
-  it('correctly renders supplementary information', () => {
+
+  it('correctly renders supplementary information', async () => {
     const suppInfo = {
-      "A11": {
-        "Key": "First Supplementary Information Commercial Transaction",
-        "Value": "1.0",
-        "Unit": "Apples"
+      A11: {
+        Key: 'First Supplementary Information Commercial Transaction',
+        Value: '1.0',
+        Unit: 'Apples',
       },
-      "A96": {
-        "Key": "Last Supplementary Information Commercial Transaction",
-        "Value": "A96"
-      }
-    }
-    const i18n = new Translate(['EN', 'DE']);
+      A96: {
+        Key: 'Last Supplementary Information Commercial Transaction',
+        Value: 'A96',
+      },
+    };
+    const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
+    const i18n = new Translate(translations);
     const supplementarInforamtion = supplementaryInformation(suppInfo, i18n);
     expect(supplementarInforamtion.length).to.be.equal(3);
     expect(supplementarInforamtion[0]).to.be.eql([
-      { text: 'Supplementary information / Ergänzende Angaben', style: 'h2', colSpan: 3 }, {}, {},
+      { text: 'Supplementary information / Ergänzende Angaben', style: 'h2', colSpan: 3 },
+      {},
+      {},
     ]);
     expect(supplementarInforamtion[1]).to.be.eql([
-      { text: 'First Supplementary Information Commercial Transaction', style: 'p', colSpan: 1 }, {text: "1.0 Apples", colSpan: 2},
+      { text: 'First Supplementary Information Commercial Transaction', style: 'tableHeader', colSpan: 1 },
+      { text: '1.0 Apples', style: 'p', colSpan: 2 },
     ]);
     expect(supplementarInforamtion[2]).to.be.eql([
-      { text: 'Last Supplementary Information Commercial Transaction', style: 'p', colSpan: 1 }, {text: "A96 ", colSpan: 2},
+      { text: 'Last Supplementary Information Commercial Transaction', style: 'tableHeader', colSpan: 1 },
+      { text: 'A96 ', style: 'p', colSpan: 2 },
     ]);
   });
-  it('correctly renders supplementary information for colSpan = 4', () => {
+
+  it('correctly renders supplementary information for colSpan = 4', async () => {
     const suppInfo = {
-      "A11": {
-        "Key": "First Supplementary Information Commercial Transaction",
-        "Value": "1.0",
-        "Unit": "Apples"
+      A11: {
+        Key: 'First Supplementary Information Commercial Transaction',
+        Value: '1.0',
+        Unit: 'Apples',
       },
-    }
-    const i18n = new Translate(['EN', 'DE']);
+    };
+    const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
+    const i18n = new Translate(translations);
     const supplementarInforamtion = supplementaryInformation(suppInfo, i18n, 4);
     expect(supplementarInforamtion.length).to.be.equal(2);
     expect(supplementarInforamtion[0]).to.be.eql([
-      { text: 'Supplementary information / Ergänzende Angaben', style: 'h2', colSpan: 4 }, {}, {}, {},
+      { text: 'Supplementary information / Ergänzende Angaben', style: 'h2', colSpan: 4 },
+      {},
+      {},
+      {},
     ]);
     expect(supplementarInforamtion[1]).to.be.eql([
-      { text: 'First Supplementary Information Commercial Transaction', style: 'p', colSpan: 2 }, {}, {text: "1.0 Apples", colSpan: 2},
+      { text: 'First Supplementary Information Commercial Transaction', style: 'tableHeader', colSpan: 2 },
+      {},
+      { text: '1.0 Apples', style: 'p', colSpan: 2 },
     ]);
   });
-})
+});
