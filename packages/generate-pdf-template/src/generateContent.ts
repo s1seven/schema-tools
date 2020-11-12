77e6b594917d6c19bfed8af923b1590de548a016
getlarge <ed@getlarge.eu>
2020-11-12 12:30:08 +0100

    fix: forget loading of translations from generateContent

--- packages/generate-pdf-template/src/generateContent.ts
+++ packages/generate-pdf-template/src/generateContent.ts
@@ -1,8 +1,6 @@
-import 'regenerator-runtime/runtime';
 import { createTransactionParties } from './lib/createTransactionParties';
 import { createCommercialTransaction } from './lib/commercialTransaction';
 import { createProductDescription } from './lib/createProductDescription';
-// import { getTranslations } from './lib/helpers';
 import { Translate } from './lib/translate';
 import { Certificate, Content } from './types';
 
@@ -10,8 +8,6 @@ export async function generateContent(
   certificate: Certificate,
   translations: Record<string, unknown>,
 ): Promise<Content> {
-  // TODO: update build config to do :
-  // ? const translations = await getTranslations(certificate.Certificate.CertificateLanguages, certificate.RefSchemaUrl);
   const i18n = new Translate(translations);
   const commercialParties = createTransactionParties(certificate.Certificate.CommercialTransaction, i18n);
   const commercialTransaction = createCommercialTransaction(certificate.Certificate.CommercialTransaction, i18n);
