37ab85ffa326cb34bcf4d28686c1e96c2741dbab
getlarge <ed@getlarge.eu>
2020-11-16 10:23:54 +0100

    add OtherTests section

--- packages/generate-pdf-template/src/generateContent.ts
+++ packages/generate-pdf-template/src/generateContent.ts
@@ -5,6 +5,7 @@ import { createProductDescription } from './lib/createProductDescription';
 import { Translate } from './lib/translate';
 import { Certificate, Content } from './types';
 import { createValidation } from './lib/createValidation';
+import { createOtherTests } from './lib/createOtherTests';
 
 export async function generateContent(
   certificate: Certificate,
@@ -15,7 +16,10 @@ export async function generateContent(
   const commercialTransaction = createCommercialTransaction(certificate.Certificate.CommercialTransaction, i18n);
   const productDescription = createProductDescription(certificate.Certificate.ProductDescription, i18n);
   const inspection = createInspection(certificate.Certificate.Inspection, i18n);
+  const otherTests = certificate.Certificate.OtherTests
+    ? createOtherTests(certificate.Certificate.OtherTests, i18n)
+    : '';
   const validation = createValidation(certificate.Certificate.Validation, i18n);
 
-  return [commercialParties, commercialTransaction, productDescription, ...inspection, ...validation];
+  return [commercialParties, commercialTransaction, productDescription, ...inspection, otherTests, ...validation];
 }
