dba1f05e712e1ec8b3b0e06b8fbc742183f4e6e8
getlarge <ed@getlarge.eu>
2020-11-16 16:36:00 +0100

    update generate-pdf module

--- packages/generate-pdf/src/index.ts
+++ packages/generate-pdf/src/index.ts
@@ -4,51 +4,68 @@ import {
   getTranslations,
   getSchemaConfig,
 } from '@s1seven/schema-tools-utils';
-import { BaseCertificateSchema, Translations } from '@s1seven/schema-tools-types';
-import merge from 'lodash.merge';
+import { ECoCSchema, EN10168Schema, Translations } from '@s1seven/schema-tools-types';
 import htmlToPdfmake from 'html-to-pdfmake';
 import jsdom from 'jsdom';
+import merge from 'lodash.merge';
 import Module from 'module';
 import PdfPrinter from 'pdfmake';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-import { Content, TDocumentDefinitions, TFontDictionary } from 'pdfmake/interfaces';
+import { Content, StyleDictionary, TDocumentDefinitions, TFontDictionary } from 'pdfmake/interfaces';
 import { URL } from 'url';
 import vm from 'vm';
 
 export type GeneratePdfOptions = {
-  inputType?: 'html' | 'json';
+  inputType?: 'json' | 'html';
   outputType?: 'buffer' | 'stream';
   templatePath?: string;
-  docDefinition?: TDocumentDefinitions;
+  docDefinition?: Partial<TDocumentDefinitions>;
   fonts?: TFontDictionary;
 };
 
-const baseDocDefinition = (pdfMakeContent: TDocumentDefinitions['content']): TDocumentDefinitions => ({
+const fonts = {
+  Lato: {
+    normal: 'node_modules/lato-font/fonts/lato-normal/lato-normal.woff',
+    bold: 'node_modules/lato-font/fonts/lato-bold/lato-bold.woff',
+    italics: 'node_modules/lato-font/fonts/lato-light-italic/lato-light-italic.woff',
+    light: 'node_modules/lato-font/fonts/lato-light/lato-light.woff',
+  },
+};
+
+const generatePdfOptions: GeneratePdfOptions = {
+  inputType: 'json',
+  outputType: 'buffer',
+};
+
+const baseDocDefinition = (content: TDocumentDefinitions['content']): TDocumentDefinitions => ({
   pageSize: 'A4',
   pageMargins: [20, 20, 20, 40],
-  content: [pdfMakeContent],
+  content,
+  defaultStyle: {
+    font: 'Lato',
+  },
 });
 
 async function buildModule(
-  refSchemaUrl: string,
-): Promise<{ generateContent: (certificate: BaseCertificateSchema, translations: Translations) => Content }> {
-  const code = (await loadExternalFile(refSchemaUrl, 'text')) as string;
-  const _module = new Module(refSchemaUrl);
-  _module.filename = refSchemaUrl;
-  (_module as any)._compile(code, refSchemaUrl);
+  refSchemaUrl: URL,
+): Promise<{ generateContent: (certificate: EN10168Schema | ECoCSchema, translations: Translations) => Content }> {
+  const code = (await loadExternalFile(refSchemaUrl.href, 'text')) as string;
+  const fileName = refSchemaUrl.pathname;
+  const _module = new Module(fileName);
+  _module.filename = fileName;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (_module as any)._compile(code, fileName);
   return _module.exports;
 }
 
 async function generateInSandbox(
-  certificate: BaseCertificateSchema,
+  certificate: EN10168Schema | ECoCSchema,
   translations: Record<string, unknown>,
 ): Promise<Content> {
   const refSchemaUrl = new URL(certificate.RefSchemaUrl);
   const [, schemaType, version] = refSchemaUrl.pathname.split('/');
   refSchemaUrl.pathname = `/${schemaType}/${version}/generate-pdf.min.js`;
 
-  const { generateContent } = await buildModule(refSchemaUrl.href);
+  const { generateContent } = await buildModule(refSchemaUrl);
   const code = `(async function () {
     content = await generateContent(certificate, translations);
   }())`;
@@ -66,51 +83,65 @@ async function generateInSandbox(
   return content;
 }
 
+function getPdfMakeContentFromHTML(certificate: string): TDocumentDefinitions['content'] {
+  const { JSDOM } = jsdom;
+  const dom = new JSDOM('');
+  return htmlToPdfmake(certificate, { window: dom.window });
+}
+
+async function getPdfMakeContentFromObject(
+  certificate: EN10168Schema | ECoCSchema,
+): Promise<TDocumentDefinitions['content']> {
+  const refSchemaUrl = new URL(certificate.RefSchemaUrl);
+  const schemaConfig = getSchemaConfig(refSchemaUrl);
+  const certificateLanguages = getCertificateLanguages(certificate);
+  const translations = certificateLanguages ? await getTranslations(certificateLanguages, schemaConfig) : {};
+  return generateInSandbox(certificate, translations);
+}
+
+async function getPdfMakeStyles(certificate: EN10168Schema | ECoCSchema): Promise<StyleDictionary> {
+  const refSchemaUrl = new URL(certificate.RefSchemaUrl);
+  const [, schemaType, version] = refSchemaUrl.pathname.split('/');
+  refSchemaUrl.pathname = `/${schemaType}/${version}/generate-pdf.styles.json`;
+  return (await loadExternalFile(refSchemaUrl.href, 'json')) as StyleDictionary;
+}
+
 export async function generatePdf(
-  certificateInput: string | Record<string, unknown>,
-  options: GeneratePdfOptions = { inputType: 'html', outputType: 'buffer' },
+  certificateInput: Record<string, unknown> | string,
+  options?: GeneratePdfOptions,
 ): Promise<Buffer | PDFKit.PDFDocument> {
-  let rawCert: any;
-  if (typeof certificateInput === 'string') {
-    rawCert = (await loadExternalFile(certificateInput, 'json')) as any;
-  } else if (typeof certificateInput === 'object') {
-    rawCert = certificateInput;
-  } else {
-    throw new Error(`Invalid input type : ${typeof certificateInput}`);
-  }
+  const opts = options ? merge(generatePdfOptions, options || {}) : generatePdfOptions;
 
   let pdfMakeContent: TDocumentDefinitions['content'];
-  if (options.inputType === 'html') {
-    const { JSDOM } = jsdom;
-    const dom = new JSDOM('');
-    pdfMakeContent = htmlToPdfmake(rawCert, { window: dom.window });
-  } else if (options.inputType === 'json') {
-    const refSchemaUrl = new URL(rawCert.RefSchemaUrl);
-    const schemaConfig = getSchemaConfig(refSchemaUrl);
-    const certificateLanguages = getCertificateLanguages(rawCert);
-    const translations = certificateLanguages ? await getTranslations(certificateLanguages, schemaConfig) : {};
-    pdfMakeContent = await generateInSandbox(rawCert, translations);
+  if (opts.inputType === 'html') {
+    pdfMakeContent = getPdfMakeContentFromHTML(certificateInput as string);
+  } else if (opts.inputType === 'json') {
+    let rawCert: EN10168Schema | ECoCSchema;
+    if (typeof certificateInput === 'string') {
+      rawCert = (await loadExternalFile(certificateInput, 'json')) as EN10168Schema | ECoCSchema;
+    } else if (typeof certificateInput === 'object') {
+      rawCert = certificateInput as EN10168Schema | ECoCSchema;
+    } else {
+      throw new Error(`Invalid certificate type : ${typeof certificateInput}`);
+    }
+    pdfMakeContent = await getPdfMakeContentFromObject(rawCert as EN10168Schema | ECoCSchema);
+    if (!opts.docDefinition.styles) {
+      opts.docDefinition.styles = await getPdfMakeStyles(rawCert);
+    }
   } else {
     throw new Error('Invalid inputType');
   }
 
-  const docDefinition: TDocumentDefinitions = options.docDefinition
-    ? merge(options.docDefinition, baseDocDefinition(pdfMakeContent))
+  const docDefinition: TDocumentDefinitions = opts.docDefinition
+    ? merge(opts.docDefinition, baseDocDefinition(pdfMakeContent))
     : baseDocDefinition(pdfMakeContent);
 
-  // console.log('PDF generation', docDefinition);
-  let pdfDoc: PDFKit.PDFDocument;
-  if (options.fonts) {
-    const printer = new PdfPrinter(options.fonts);
-    pdfDoc = printer.createPdfKitDocument(docDefinition);
-  } else {
-    pdfMake.vfs = pdfFonts.pdfMake.vfs;
-    pdfDoc = pdfMake.createPdf(docDefinition).getStream();
-  }
+  const printer = new PdfPrinter(opts.fonts || fonts);
+  const pdfDoc = printer.createPdfKitDocument(docDefinition);
 
-  if (options.outputType === 'stream') {
+  if (opts.outputType === 'stream') {
     return pdfDoc;
-  } else if (options.outputType === 'buffer') {
+  } else if (opts.outputType === 'buffer') {
     return new Promise((resolve, reject) => {
       let buffer: Buffer = Buffer.alloc(0);
       pdfDoc.on('data', (data) => {
@@ -122,6 +153,7 @@ export async function generatePdf(
       pdfDoc.on('error', (error) => {
         reject(error);
       });
+      pdfDoc.end();
     });
   }
 
