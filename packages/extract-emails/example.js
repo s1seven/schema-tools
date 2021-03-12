08a89ed0db03bc165ed2ff8480474e912e70ad2c
getlarge <ed@getlarge.eu>
2021-03-12 14:02:28 +0100

    fix: add example

---
+++ packages/extract-emails/example.js
@@ -0,0 +1,20 @@
+const { extractEmails } = require('./dist/index');
+const en10168Certificate = require('../../fixtures/EN10168/v0.0.2/valid_cert.json');
+const eCoCCertificate = require('../../fixtures/E-CoC/v0.0.2-2/valid_cert.json');
+
+(async function (argv) {
+  try {
+    const schemaType = argv[2] || 'en10168-schemas';
+
+    let mails;
+    if (schemaType.startsWith('en10168')) {
+      mails = await extractEmails(en10168Certificate);
+    } else if (schemaType.startsWith('e-coc')) {
+      mails = await extractEmails(eCoCCertificate);
+    }
+
+    console.log({ mails })
+  } catch (error) {
+    console.error(error.message);
+  }
+})(process.argv);
