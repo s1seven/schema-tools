20a07215646705390262482fd4dbfbc9c90d1150
getlarge <ed@getlarge.eu>
2021-06-29 16:13:15 +0200

    feat: add support for new schemas and use class validator for prevalidation

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,30 +1,169 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsDateString,
+  IsEnum,
+  IsNotEmptyObject,
+  IsOptional,
+  IsString,
+  IsUrl,
+  IsUUID,
+  ValidateNested,
+} from 'class-validator';
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export { JSONSchema7, JSONSchema7Definition } from 'json-schema';
 
-export type SchemaTypes = 'en10168-schemas' | 'e-coc-schemas';
+export type SchemaTypes = 'en10168-schemas' | 'e-coc-schemas' | 'coa-schemas' | 'cdn-schemas';
 
-export interface BaseCertificateSchema {
+export class BaseCertificateSchema {
+  @IsUrl({ protocols: ['http', 'https'], require_tld: false, require_protocol: true })
   RefSchemaUrl: string;
+
   [key: string]: any;
 }
 
-export interface EN10168Schema extends BaseCertificateSchema {
-  DocumentMetadata?: any;
-  Certificate: {
-    CertificateLanguages: any;
-    CommercialTransaction: any;
-    ProductDescription: any;
-    Inspection: any;
-    OtherTests: any;
-    Validation: any;
-  };
+export enum CertificateDocumentMetadataState {
+  DRAFT = 'draft',
+  VALID = 'valid',
+  CANCELLED = 'cancelled',
 }
 
-export interface ECoCSchema extends BaseCertificateSchema {
+export class CertificateDocumentMetadata {
+  id: string;
+  version?: number;
+  state?: CertificateDocumentMetadataState;
+}
+
+export type Languages = 'EN' | 'DE' | 'FR' | 'PL' | 'RU';
+
+export type Translations = {
+  [key in Languages]?: Record<string, any>;
+};
+
+export enum CertificateLanguages {
+  EN = 'EN',
+  DE = 'DE',
+  FR = 'FR',
+  PL = 'PL',
+  RU = 'RU',
+}
+
+export class EN10168SchemaCertificate {
+  @IsEnum(CertificateLanguages, { each: true })
+  CertificateLanguages: CertificateLanguages[];
+
+  @IsNotEmptyObject()
+  CommercialTransaction: Record<string, any>;
+
+  @IsNotEmptyObject()
+  ProductDescription: Record<string, any>;
+
+  @IsNotEmptyObject()
+  Inspection: Record<string, any>;
+
+  @IsNotEmptyObject()
+  OtherTests: Record<string, any>;
+
+  @IsNotEmptyObject()
+  Validation: Record<string, any>;
+}
+
+export class EN10168Schema extends BaseCertificateSchema {
+  @IsOptional()
+  @IsEnum(CertificateDocumentMetadata)
+  DocumentMetadata?: CertificateDocumentMetadata;
+
+  @Type(() => EN10168SchemaCertificate)
+  @ValidateNested()
+  Certificate: EN10168SchemaCertificate;
+}
+
+export class ECoCSchema extends BaseCertificateSchema {
+  @IsString()
   Id: string;
+
+  @IsUUID()
   Uuid: string;
+
+  @IsUrl()
   URL: string;
-  EcocData: any;
+
+  @IsNotEmptyObject()
+  EcocData: Record<string, any>;
+}
+
+export class CoASchemaCertificate {
+  @IsEnum(CertificateLanguages, { each: true })
+  CertificateLanguages: CertificateLanguages[];
+
+  @IsString()
+  Number: string;
+
+  @IsDateString()
+  Date: string;
+
+  @IsOptional()
+  @IsString()
+  Type?: string;
+
+  @IsOptional()
+  @IsArray()
+  Contacts?: any;
+
+  @IsNotEmptyObject()
+  Parties: Record<string, any>;
+
+  @IsNotEmptyObject()
+  BusinessReferences: Record<string, any>;
+
+  @IsNotEmptyObject()
+  Product: Record<string, any>;
+
+  @IsNotEmptyObject()
+  Analysis: Record<string, any>;
+
+  @IsNotEmptyObject()
+  DeclarationOfConformity: Record<string, any>;
+
+  @IsOptional()
+  @IsArray()
+  Attachments?: any;
+}
+
+export class CoASchema extends BaseCertificateSchema {
+  @IsOptional()
+  @IsEnum(CertificateDocumentMetadata)
+  DocumentMetadata?: CertificateDocumentMetadata;
+
+  @Type(() => CoASchemaCertificate)
+  @ValidateNested()
+  Certificate: CoASchemaCertificate;
+}
+
+export class CDNSchemaCertificate {
+  @IsEnum(CertificateLanguages, { each: true })
+  CertificateLanguages: CertificateLanguages[];
+
+  @IsNotEmptyObject()
+  Parties: Record<string, any>;
+
+  @IsNotEmptyObject()
+  BusinessReferences: Record<string, any>;
+
+  @IsNotEmptyObject()
+  Product: Record<string, any>;
+
+  @IsArray()
+  Certificates: Record<string, any>[];
+}
+export class CDNSchema extends BaseCertificateSchema {
+  @IsOptional()
+  @IsEnum(CertificateDocumentMetadata)
+  DocumentMetadata?: CertificateDocumentMetadata;
+
+  @Type(() => CDNSchemaCertificate)
+  @ValidateNested()
+  CertificateDeliveryNote: CDNSchemaCertificate;
 }
 
 export interface ValidationError {
@@ -40,9 +179,3 @@ export interface SchemaConfig {
   schemaType: SchemaTypes;
   version: string;
 }
-
-export type Languages = 'EN' | 'DE' | 'FR' | 'PL';
-
-export type Translations = {
-  [key in Languages]?: any;
-};
