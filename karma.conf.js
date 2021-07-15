bdc3e217771307698cd3ee8e3e5526e2e2105b6a
getlarge <ed@getlarge.eu>
2021-07-15 07:28:21 +0200

    test: add global jest configs

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
