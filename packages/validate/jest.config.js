6ad7a791e74972fecc6afd5b8cc72cff53f420e6
getlarge <ed@getlarge.eu>
2021-07-15 07:53:23 +0200

    test: add package level jest config

---
+++ packages/validate/jest.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const base = require('../../jest.config.base.js');
+const pkg = require('./package.json');
+
+const projectName = pkg.name.split('@s1seven/schema-tools-').pop();
+
+module.exports = {
+  ...base,
+  name: pkg.name,
+  displayName: pkg.name,
+  rootDir: '../..',
+  testMatch: [`<rootDir>/packages/${projectName}/**/*.spec.ts`],
+  coverageDirectory: `<rootDir>/packages/${projectName}/coverage/`,
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 65,
+      functions: 65,
+      lines: 85,
+    },
+  },
+  reporters: ['default'],
+  moduleDirectories: ['node_modules'],
+};
