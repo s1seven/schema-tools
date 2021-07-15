214419d2292882d51eee25de080ce2c35bdcc831
getlarge <ed@getlarge.eu>
2021-07-15 08:31:46 +0200

    fix: remove nested template literals

--- packages/generate-EN10168-pdf-template/src/lib/measurement.ts
+++ packages/generate-EN10168-pdf-template/src/lib/measurement.ts
@@ -15,8 +15,8 @@ export function renderMeasurement(measurement: Measurement, name: string, i18n: 
         alignment: 'justify',
         columns: [
           { text: `${localizeNumber(Value, i18n.languages[0])} ${Unit ? Unit : ''}`, style: 'p' },
-          { text: `${Minimum ? `min ${localizeNumber(Minimum, i18n.languages[0])} ${Unit}` : ''}`, style: 'p' },
-          { text: `${Maximum ? `max ${localizeNumber(Maximum, i18n.languages[0])} ${Unit}` : ''}`, style: 'p' },
+          { text: Minimum ? `min ${localizeNumber(Minimum, i18n.languages[0])} ${Unit}` : '', style: 'p' },
+          { text: Maximum ? `max ${localizeNumber(Maximum, i18n.languages[0])} ${Unit}` : '', style: 'p' },
         ],
       },
     ],
