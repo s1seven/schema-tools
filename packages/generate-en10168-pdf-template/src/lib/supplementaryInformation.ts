993d34c629e264417b55a92494337b7b1485ee66
getlarge <ed@getlarge.eu>
2021-12-02 10:29:24 +0100

    feat: display `Interpretation` for other tests

--- packages/generate-en10168-pdf-template/src/lib/supplementaryInformation.ts
+++ packages/generate-en10168-pdf-template/src/lib/supplementaryInformation.ts
@@ -1,21 +1,49 @@
 import { createEmptyColumns, localizeValue, Translate } from '@s1seven/schema-tools-generate-pdf-template-helpers';
-import { CommercialTransactionSupplementaryInformation } from '../types';
+import { KeyValueObject } from '../types';
 import { TableCell } from 'pdfmake/interfaces';
 
 export const supplementaryInformation = (
-  data: CommercialTransactionSupplementaryInformation,
+  data: { [k: string]: KeyValueObject },
   i18n: Translate,
   colSpan = 3,
 ): TableCell[][] => {
-  const dataMapped = Object.keys(data).map((element) => [
-    { text: `${element} ${data[element].Key}`, style: 'tableHeader', colSpan: colSpan - 1 },
-    ...createEmptyColumns(colSpan - 2),
-    {
-      text: `${localizeValue(data[element].Value, data[element].Type, i18n.languages[0])} ${data[element].Unit || ''}`,
-      style: 'p',
-      colSpan: 1,
-    },
-  ]);
+  // TODO: to allow using intepretation in any supplementaryInformation, parent table layout will have to be adapated
+  // const dataMapped: TableCell[][] = Object.keys(data).map((element) => {
+  //   const { Interpretation, Key, Value, Type, Unit } = data[element];
+  //   const tableCells: TableCell[] = [
+  //     { text: `${element} ${Key}`, style: 'tableHeader', colSpan: colSpan - 2 },
+  //     {
+  //       text: `${localizeValue(Value, Type, i18n.languages[0])} ${Unit || ''}`,
+  //       style: 'p',
+  //       colSpan: 1,
+  //     },
+  //     {
+  //       text: Interpretation || '',
+  //       style: 'p',
+  //       colSpan: 1,
+  //     },
+  //   ];
+
+  //   if (colSpan - 3 > 0) {
+  //     createEmptyColumns(colSpan - 3).forEach((col, i) => {
+  //       tableCells.splice(1 + i, 0, col);
+  //     });
+  //   }
+  //   return tableCells;
+  // });
+
+  const dataMapped: TableCell[][] = Object.keys(data).map((element) => {
+    const { Key, Value, Type, Unit } = data[element];
+    return [
+      { text: `${element} ${Key}`, style: 'tableHeader', colSpan: colSpan - 1 },
+      ...createEmptyColumns(colSpan - 2),
+      {
+        text: `${localizeValue(Value, Type, i18n.languages[0])} ${Unit || ''}`,
+        style: 'p',
+        colSpan: 1,
+      },
+    ];
+  });
 
   if (!dataMapped?.length) return [];
   return [
