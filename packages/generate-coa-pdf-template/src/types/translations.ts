dfb4771f0df6320044dd9e01e1ec8b322a1c5070
getlarge <ed@getlarge.eu>
2022-02-16 12:39:36 +0100

    feat(generate-coa-pdf-template): add translations types

---
+++ packages/generate-coa-pdf-template/src/types/translations.ts
@@ -0,0 +1,57 @@
+export interface CoACertificateTranslations {
+  Customer: string;
+  Receiver: string;
+  Id: string;
+  Certificate: string;
+  Date: string;
+  BusinessTransaction: string;
+  Order: string;
+  OrderId: string;
+  OrderQuantity: string;
+  OrderPosition: string;
+  OrderDate: string;
+  InternalOrderId: string;
+  InternalOrderPosition: string;
+  Delivery: string;
+  DeliveryId: string;
+  DeliveryPosition: string;
+  DeliveryQuantity: string;
+  DeliveryDate: string;
+  Transport: string;
+  GoodsReceiptId: string;
+  Product: string;
+  ProductName: string;
+  ProductId: string;
+  CustomerProductId: string;
+  CustomerProductName: string;
+  CountryOfOrigin: string;
+  PlaceOfOrigin: string;
+  FillingBatchId: string;
+  FillingBatchDate: string;
+  ProductionBatchId: string;
+  ProductionDate: string;
+  ExpirationDate: string;
+  Standards: string;
+  AdditionalInformation: string;
+  Inspections: string;
+  LotId: string;
+  Property: string;
+  Method: string;
+  Value: string;
+  Minimum: string;
+  Maximum: string;
+  Unit: string;
+  TestConditions: string;
+  DeclarationOfConformity: string;
+  Contacts: string;
+  ContactName: string;
+  ContactRole: string;
+  ContactDepartment: string;
+  ContactEmail: string;
+  ContactPhone: string;
+  Attachments: string;
+}
+
+export interface CoATranslations {
+  Certificate: CoACertificateTranslations;
+}
