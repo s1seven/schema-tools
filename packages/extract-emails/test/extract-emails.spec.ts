a80212f95a022f01317a423a91041a1b02281071
getlarge <ed@getlarge.eu>
2022-02-18 06:11:43 +0100

    test(extract-emails): add tests for E-CoC v1

--- packages/extract-emails/test/extract-emails.spec.ts
+++ packages/extract-emails/test/extract-emails.spec.ts
@@ -82,6 +82,28 @@ describe('ExtractEmails', function () {
       },
     },
     {
+      version: 'v1.0.0',
+      type: SupportedSchemas.ECOC,
+      certificatePath: `${__dirname}/../../../fixtures/E-CoC/v1.0.0/valid_cert.json`,
+      certificate: require('../../../fixtures/E-CoC/v1.0.0/valid_cert.json'),
+      expectedSenders: {
+        [SenderRoles.Manufacturer]: {
+          emails: null,
+          name: 'Material Manufacturing SE',
+          role: 'Manufacturer',
+          vatId: 'AT123456789',
+        },
+      },
+      expectedReceivers: {
+        [ReceiverRoles.Recipient]: {
+          emails: null,
+          name: 'Material Trading AG',
+          role: 'Recipient',
+          vatId: 'DE12234567890',
+        },
+      },
+    },
+    {
       version: 'v0.0.4',
       type: SupportedSchemas.COA,
       certificatePath: `${__dirname}/../../../fixtures/CoA/v0.0.4/valid_cert.json`,
