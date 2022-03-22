15f246490f679f0dca261b560ed86c72478ab9de
getlarge <ed@getlarge.eu>
2022-03-22 19:10:23 +0100

    test(generate-pdf-template-helpers): update translate unit tests

--- packages/generate-pdf-template-helpers/test/translate.spec.ts
+++ packages/generate-pdf-template-helpers/test/translate.spec.ts
@@ -1,7 +1,10 @@
 import { readFileSync } from 'fs';
 
+import { ExternalStandardsEnum, ExtraTranslations, Translations } from '@s1seven/schema-tools-types';
+
 import { Translate } from '../src';
 import { getTranslations } from './getTranslations';
+
 const extraTranslations = JSON.parse(readFileSync('../../fixtures/CoA/v0.1.1/extraTranslations.json', 'utf8'));
 
 describe('Translate', () => {
@@ -30,28 +33,50 @@ describe('Translate', () => {
     });
   });
 
-  describe('CoA certificate CAMPUS', () => {
+  describe('CoA certificate', () => {
     const defaultSchemaUrl = 'https://schemas.s1seven.com/coa-schemas/v0.1.0/schema.json';
 
-    it('correctly translate a Property into 2 languages', async () => {
+    it('correctly translate Certificate into 2 languages', async () => {
+      const translations = await getTranslations(['DE', 'EN'], defaultSchemaUrl);
+      const i18n = new Translate(translations, {}, ['DE', 'EN']);
+      const translation = i18n.translate('Customer', 'Certificate');
+      expect(translation).toEqual('Kunde / Customer');
+    });
+
+    it('correctly translate Certificate into 1 language', async () => {
+      const translations = await getTranslations(['FR'], defaultSchemaUrl);
+      const i18n = new Translate(translations, {}, ['FR']);
+      const translation = i18n.translate('Customer', 'Certificate');
+      expect(translation).toEqual('Client');
+    });
+
+    it('correctly translate a Property into 2 languages when externalStandard is CAMPUS', async () => {
       const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
       const i18n = new Translate(translations, extraTranslations, ['EN', 'DE']);
-      const translation = i18n.extraTranslate('CAMPUS', '1', 'Property', 'Yield stress');
+      const translation = i18n.extraTranslate(ExternalStandardsEnum.CAMPUS, '1', 'Property', 'Yield stress');
       expect(translation).toEqual('Yield stress / Streckspannung');
     });
 
-    it('correctly translate a Property into 2 languages', async () => {
+    it('correctly translate a Property into 2 languages when externalStandard is CAMPUS', async () => {
       const translations = await getTranslations(['DE'], defaultSchemaUrl);
       const i18n = new Translate(translations, extraTranslations, ['DE']);
-      const translation = i18n.extraTranslate('CAMPUS', '1', 'Property', 'Yield stress');
+      const translation = i18n.extraTranslate(ExternalStandardsEnum.CAMPUS, '1', 'Property', 'Yield stress');
       expect(translation).toEqual('Streckspannung');
     });
 
-    it('correctly translate a TestCondition into 2 languages', async () => {
+    it('correctly translate a TestCondition into 2 languages when externalStandard is CAMPUS', async () => {
       const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
-      const i18n = new Translate(translations, extraTranslations, ['EN', 'DE']);
-      const translation = i18n.extraTranslate('CAMPUS', '785', 'TestConditions', 'of test plate');
+      const i18n = new Translate<Translations, ExtraTranslations>(translations, extraTranslations, ['EN', 'DE']);
+      const translation = i18n.extraTranslate(ExternalStandardsEnum.CAMPUS, '785', 'TestConditions', 'of test plate');
       expect(translation).toEqual('of test plate / der Testplatte');
+    });
+
+    it('returns the default translation when externalStandard is undefined', async () => {
+      const translations = await getTranslations(['EN', 'DE'], defaultSchemaUrl);
+      const defaultTranslation = 'of test';
+      const i18n = new Translate<Translations, ExtraTranslations>(translations, extraTranslations, ['EN', 'DE']);
+      const translation = i18n.extraTranslate(undefined, '785', 'TestConditions', defaultTranslation);
+      expect(translation).toEqual(defaultTranslation);
     });
   });
 });
