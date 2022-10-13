52cf7e03c8d1844f32e2a749a6c1e7bb7b5e8073
Eamon <eamon0989@gmail.com>
2022-10-13 16:56:45 +0200

    chore: update extract-emails example script

--- packages/extract-emails/example.js
+++ packages/extract-emails/example.js
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { extractEmails } = require('./dist/index');
-const en10168Certificate = require('../../fixtures/EN10168/v0.0.2/valid_cert.json');
-const eCoCCertificate = require('../../fixtures/E-CoC/v0.0.2-2/valid_cert.json');
+const { extractParties } = require('./dist/index');
+const en10168Certificate = require('../../fixtures/EN10168/v0.3.0/valid_cert.json');
+const coaCertificate = require('../../fixtures/CoA/v1.0.0/valid_cert.json');
+const eCoCCertificate = require('../../fixtures/E-CoC/v1.0.0/valid_cert.json');
 
 (async function (argv) {
   try {
@@ -9,9 +10,11 @@ const eCoCCertificate = require('../../fixtures/E-CoC/v0.0.2-2/valid_cert.json')
 
     let mails;
     if (schemaType.startsWith('en10168')) {
-      mails = await extractEmails(en10168Certificate);
+      mails = await extractParties(en10168Certificate);
     } else if (schemaType.startsWith('e-coc')) {
-      mails = await extractEmails(eCoCCertificate);
+      mails = await extractParties(eCoCCertificate);
+    } else if (schemaType.startsWith('coa')) {
+      mails = await extractParties(coaCertificate);
     }
 
     console.log({ mails });
