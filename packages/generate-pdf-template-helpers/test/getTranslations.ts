34a4552a764eafa919d9c40e30626b3a1706772a
Eamon <eamon0989@gmail.com>
2022-10-14 11:28:41 +0200

    test: update generate-pdf-template-helpers tests to use latest coa and en10168 certs

--- packages/generate-pdf-template-helpers/test/getTranslations.ts
+++ packages/generate-pdf-template-helpers/test/getTranslations.ts
@@ -1,10 +1,23 @@
 import { URL } from 'url';
 
-import { getSchemaConfig, getTranslations as loadTranslations } from '@s1seven/schema-tools-utils';
+import { CampusTranslations } from '@s1seven/schema-tools-types';
+import {
+  getExtraTranslations as loadExtraTranslations,
+  getSchemaConfig,
+  getTranslations as loadTranslations,
+} from '@s1seven/schema-tools-utils';
 
 import { Translations } from '../src/types';
 
 export async function getTranslations(certificateLanguages: string[], refSchemaUrl: string) {
   const schemaConfig = getSchemaConfig(new URL(refSchemaUrl));
   return loadTranslations(certificateLanguages, schemaConfig) as Promise<Translations>;
+}
+export function getExtraTranslations(
+  certificateLanguages: string[],
+  refSchemaUrl: string,
+  externalStandards: 'CAMPUS'[],
+) {
+  const schemaConfig = getSchemaConfig(new URL(refSchemaUrl));
+  return loadExtraTranslations(certificateLanguages, schemaConfig, externalStandards) as Promise<CampusTranslations>;
 }
