a55ba113f0e117cf95c9df93fb06d2046d764727
christophbuehler <christoph@cbuehler.li>
2024-02-23 14:36:45 +0100

    Add attachCertificateToPdf and attachFileToPdf functions

--- packages/generate-pdf/src/pdf-a-3a/attach-file-to-pdf.ts
+++ packages/generate-pdf/src/pdf-a-3a/attach-file-to-pdf.ts
@@ -1,7 +1,7 @@
-import { AttachmentOptions, PDFDocument } from 'pdf-lib';
+import { AFRelationship, AttachmentOptions, PDFDocument } from 'pdf-lib';
 
 /**
- * Attaches a file to a PDF document. The file can be viewed and downloaded in advanced PDF viewers like Adobe Acrobat Reader.
+ * Attach a file to a PDF document. The file can be viewed and downloaded in advanced PDF viewers like Adobe Acrobat Reader.
  * @param pdfBuffer - The buffer of the PDF to which the file will be attached.
  * @param fileContent - The content of the file to attach.
  * @param fileName - The name of the file to attach.
@@ -9,14 +9,14 @@ import { AttachmentOptions, PDFDocument } from 'pdf-lib';
  * @returns A Promise that resolves with a Buffer of the updated PDF document.
  */
 export const attachFileToPdf = async (
-  pdfBuffer: Buffer,
+  pdfLibDoc: PDFDocument,
   fileContent: string,
   fileName: string,
   options: AttachmentOptions,
-): Promise<Buffer> => {
-  const pdfDoc = await PDFDocument.load(pdfBuffer);
-  const encodedFileContent = Buffer.from(fileContent).toString('base64');
-  await pdfDoc.attach(encodedFileContent, fileName, options);
-  const uint8Array = await pdfDoc.save();
-  return Buffer.from(uint8Array);
+): Promise<void> => {
+  const encodedFileContent = Buffer.from(fileContent, 'utf-8').toString('base64');
+  await pdfLibDoc.attach(encodedFileContent, fileName, {
+    ...options,
+    afRelationship: AFRelationship.Source,
+  });
 };
