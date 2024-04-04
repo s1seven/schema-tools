83ea2fc88f85fbb6f9ec897693b1587e045f016f
christophbuehler <christoph@cbuehler.li>
2024-04-04 14:29:40 +0200

    Add lodash.cloneDeep for cloning the certificate object

--- packages/generate-pdf/src/index.ts
+++ packages/generate-pdf/src/index.ts
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash';
 import clone from 'lodash.clone';
 import get from 'lodash.get';
 import merge from 'lodash.merge';
@@ -71,11 +72,13 @@ export async function generatePdf(certificate: Schemas | string, options: Genera
 
   options.fonts ??= defaultFonts;
 
-  const pdfMakeContent = await buildPdfContent(jsonCertificate, options);
+  const certificateCloneForPdfMake = cloneDeep(jsonCertificate);
+  const pdfMakeContent = await buildPdfContent(certificateCloneForPdfMake, options);
   const docDefinition: TDocumentDefinitions = merge(
     baseDocDefinition(pdfMakeContent),
     clone(options.docDefinition ?? {}),
   );
+
   const printer = new PdfPrinter(options.fonts);
   const pdfDoc = printer.createPdfKitDocument(docDefinition);
   const pdfBuffer = await pdfDocToBuffer(pdfDoc);
